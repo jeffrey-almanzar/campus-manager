e7c492512e1b235f22d09ea4fe702c236895b53e
Asifa Khan <[asifakhan00011@gmail.com]>
2019-06-10 15:40:56 -0400

    initial commit

---
+++ src/CampusCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import StudentRow from './StudentRow';
+
+class CampusCard extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+    students: ['jeff a', 'juan pablo', 'Pedro']}
+
+  }
+
+  getStudents(){
+    let students =[];
+    this.state.students.forEach( (item) =>{
+      students.push(<StudentRow name={item} />)
+    })
+
+    return students;
+  }
+
+  render(){
+    return (
+      <div>
+         {this.getStudents()}
+         
+      </div>
+     
+    )
+  }
+}
+
+export default CampusCard;
