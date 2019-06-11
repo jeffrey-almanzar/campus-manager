e5729ef9feb82857e856f24bcc5e3e770f4e0640
Asifa Khan <[asifakhan00011@gmail.com]>
2019-06-11 17:18:59 -0400

    all cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import CampusCard from './CampusCard';
 import StudentCard from './StudentCard';
 import ShowCampus from './ShowCampus';
-// import ShowStudent from '/ShowStudent';
+import ShowStudent from './ShowStudent';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       < CampusCard campName = "Lehman College"/>
       < StudentCard name = "Alice Wang" campName ="Lehman College"/>
       < ShowCampus campName = "Lehman College"/>
+      < ShowStudent name= "Alice" gpa="3.5" campName ="Hostos"/>
       
     </div>
   );
--- src/ShowStudent.js
+++ src/ShowStudent.js
@@ -5,59 +5,81 @@ import StudentCard from'./StudentCard'
 
 class ShowStudent extends React.Component{
 
-  showStudentCard(){
-    let studentCard= []
-    for(let i =0; i<10; i++){
-      studentCard.push(<StudentCard name ="Ada" campName ="Lehman College" /> )
-    }
-
-    return studentCard;
-  }
 
   render(){
     return (
-      <div>
-      <div className="image-showCampus">
-           <img src="https://www.uri.edu/wp-content/uploads/home/2018/05/kingston-campus-500x327.jpg" alt="lehman img"/>
-      <div>
 
-          <h1>{this.props.campName} </h1>
-        
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. </p>
-      </div>  
-      </div>
-      <div className="address">
-        <div>
-           <p>250 Bedford Park Blvd W</p>
-           <p>The Bronx, NY 10468</p>
+     
+      <div>
+        <h1>Show Student</h1>
+
+
+        <div className="showStudent">
+          <div >
+            <img src="https://next.bncollege.com/wp-content/uploads/2016/10/Student-with-Textbook.jpg" alt="lehman img"/>
+          </div>
+
+          <div className="student-info-container">
+              <h1>Name: {this.props.name} </h1>
+              <p>GPA: {this.props.gpa}</p>
+               <div className="button2">
+                 <button>Edit</button>
+                 <button>Delete</button>
+             </div> 
+          </div>
         </div>
 
+
+<h2>This student is registered in a campus</h2>
+
+    <div className= "changeCampus">
+        <div className="image-c">
+            <div>
+              <img src="https://www.uri.edu/wp-content/uploads/home/2018/05/kingston-campus-500x327.jpg" alt="lehman img"/>
+            </div>
+
+            <div>
+              <h1> {this.props.campName} </h1>
+              <p>5 Students</p>
+              <div className="campButton">
+              <button>Edit</button>
+              </div>
+            </div>
+        </div>
+
+
         <div>
-           <button>Edit</button>
-           <button>Delete</button>
-        </div> 
-      </div>
+           <select>
+          <option value="">Select Campus</option>
+          <option value="hostos">Hostos</option>
+          <option value="hunter">Hunter</option>
+          <option value="bmcc">BMCC</option>
+          </select>
+          <br></br>
+          <br></br>
+          <br></br>
+          
+           <button>Change Campus</button>
 
-    <div className="studentList">
-      <div>
-        <h1>Students On Campus</h1>
-      </div>
-      <div>
-      <button>Add Student</button>
-      </div>
-    </div>
+
+        </div>
+</div>
+
+          
+        
+       
+        
+
+
       
-      <div className= "studentCards">
-        {this.showStudentCard()}
+
 
       </div>
       
       
       
-      </div>
+      
+     
 );
 }
 };
