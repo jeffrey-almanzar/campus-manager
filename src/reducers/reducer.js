89d56b9e31e3be4cb36ccb9e3e99b50381bcdc17
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-07-18 23:21:19 -0400

    created requestStudents action and reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -151,3 +151,26 @@ export const requestCampuses = (state= requestCampusesInitState, action={}) =>{
            return state;
     }
 }
+
+const requestStudentsInitState= {
+    isPending:false,
+    students:[],
+    error:""
+ }
+
+export const requestCampuses = (state= requestCampusesInitState, action={}) =>{
+    switch(action.type){
+       case "REQUEST_STUDENTS_PENDING":
+         return Object.assign({}, state, {isPending:true});
+
+       case "REQUEST_STUDENTS_SUCCESS":
+           
+        return {isPending:false, students:[...action.payload] };
+
+       case "REQUEST_STUDENTS_FAILED":
+        return Object.assign({}, state, {isPending:false, error:action.payload});
+
+       default:
+           return state;
+    }
+}
